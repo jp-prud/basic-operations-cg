aba5fe2521df1651288cf0926f4a88a0de75a746
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2025-03-25 22:54:06 +0000

    feat: imp default actions

--- ComputacaoGrafica/ComputacaoGrafica.cpp
+++ ComputacaoGrafica/ComputacaoGrafica.cpp
@@ -1,164 +1,151 @@
 #include <iostream>
 #include <GL/freeglut.h>
 #include <vector>
-using vertice = std::pair<double, double>;
+#include <cmath>
+
+using vertice = std::tuple<double, double, double>;
 using lista_vertices = std::vector<vertice>;
 using aresta = std::pair<int, int>;
 using lista_arestas = std::vector<aresta>;
 
-struct Poligono {
-	double tamanhoLado;
-	int numLados;
-	vertice posicao;
-	vertice escala;
-	double rotacao;
-	lista_vertices vertices;
-	lista_arestas arestas;
+struct Cubo {
+    vertice posicao;
+    vertice escala;
+    double rotacaoX, rotacaoY;
+    lista_vertices vertices;
+    lista_arestas arestas;
 };
 
-Poligono criar_poligono(double posicao_x, double posicao_y, double tamanho_lado, int num_lados);
-void desenhar(Poligono poligono);
-void movimentar(Poligono& poligono, double distancia, double angulo);
-void escalar(Poligono& poligono, double escala_x, double escala_y);
-void rotacionar(Poligono& poligono, double angulo);
+Cubo criar_cubo(double x, double y, double z, double tamanho);
+void desenhar(Cubo cubo);
+void movimentar(Cubo& cubo, double dx, double dy, double dz);
+void escalar(Cubo& cubo, double sx, double sy, double sz);
+void rotacionar(Cubo& cubo, double anguloX, double anguloY);
 void display();
-void redraw(int value);
 void keyboard(unsigned char key, int x, int y);
 void keyboard_special(int key, int x, int y);
+void redraw(int value);
 
-Poligono pentagono;
+Cubo cubo;
 int delay = 10;
 
+glutInitWindowSize(512, 512);
 int main(int argc, char** argv) {
-
-	pentagono = criar_poligono(128, 128, 30, 5);
-
-	glutInit(&argc, argv);
-
-	glutInitWindowSize(512, 512);
-
-	glutCreateWindow("Desenhando uma linha");
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glOrtho(0, 256, 0, 256, -1, 1);
-
-	glutDisplayFunc(display);
-	glutKeyboardFunc(keyboard);
-	glutSpecialFunc(keyboard_special);
-	glutTimerFunc(10, redraw, 0);
-
-	glutMainLoop();
-
-	return 0;
+    cubo = criar_cubo(0, 0, 0, 50);
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutCreateWindow("Cubo Wireframe");
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glOrtho(-100, 100, -100, 100, -100, 100);
+    
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutSpecialFunc(keyboard_special);
+    glutTimerFunc(delay, redraw, 0);
+    
+    glutMainLoop();
+    return 0;
 }
 
-
-void display(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	desenhar(pentagono);
-
-	glFlush();
-}
-
-void keyboard(unsigned char key, int x, int y) {
-	std::cout << key;
-	switch (key) {
-	case 27:
-		exit(0);
-		break;
-	case ' ':
-		escalar(pentagono, 1.1, 1.1);
-		break;
-	}
-}
-
-void keyboard_special(int key, int x, int y) {
-	std::cout << key;
-	switch (key) {
-	case GLUT_KEY_DOWN:
-		movimentar(pentagono, 10, (270 / 180.0) * 3.1416);
-		break;
-
-	case GLUT_KEY_UP:
-		movimentar(pentagono, 10, (90 / 180.0) * 3.1416);
-		break;
-
-	case GLUT_KEY_RIGHT:
-		movimentar(pentagono, 10, (0 / 180.0) * 3.1416);
-		break;
-
-	case GLUT_KEY_LEFT:
-		movimentar(pentagono, 10, (180 / 180.0) * 3.1416);
-		break;
-
-	}
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    desenhar(cubo);
+    glFlush();
 }
 
 void redraw(int value) {
-	glutPostRedisplay();
-	glutTimerFunc(delay, redraw, 0);
+    glutPostRedisplay();
+    glutTimerFunc(delay, redraw, 0);
 }
 
-Poligono criar_poligono(double posicao_x, double posicao_y, double tamanho_lado, int num_lados) {
-	Poligono novo_poligono;
-	novo_poligono.numLados = num_lados;
-	novo_poligono.posicao.first = posicao_x;
-	novo_poligono.posicao.second = posicao_y;
-	novo_poligono.tamanhoLado = tamanho_lado;
-
-	novo_poligono.escala.first = 1;
-	novo_poligono.escala.second = 1;
-
-	novo_poligono.rotacao = 0;
-
-	float angulo = 0;
-	float passo_angulo = ((360 / float(num_lados)) * 3.1415926536) / 180.0;
-
-	float apothem = tamanho_lado / (2 * tan(3.1416 / float(num_lados)));
-	posicao_x -= tamanho_lado / 2.0;
-	posicao_y -= apothem;
-
-	novo_poligono.vertices.push_back(vertice(posicao_x, posicao_y));
-	std::cout << "Vertices:\n";
-	std::cout << 0 << " - " << posicao_x << " - " << posicao_y << "\n";
-	for (int i = 1; i < num_lados; i++) {
-		posicao_x = posicao_x + tamanho_lado * cos(angulo);
-		posicao_y = posicao_y + tamanho_lado * sin(angulo);
-		novo_poligono.vertices.push_back(vertice(posicao_x, posicao_y));
-		std::cout << i << " - " << posicao_x << " - " << posicao_y << "\n";
-		angulo += passo_angulo;
-	}
-
-	std::cout << "Arestas:\n";
-	for (int i = 0; i < num_lados; i++) {
-		novo_poligono.arestas.push_back(aresta(i, (i + 1) % num_lados));
-		std::cout << i << " - " << (i + 1) % num_lados << "\n";
-	}
-
-
-	return novo_poligono;
+Cubo criar_cubo(double x, double y, double z, double tamanho) {
+    Cubo novo_cubo;
+    novo_cubo.posicao = { x, y, z };
+    novo_cubo.escala = { 1, 1, 1 };
+    novo_cubo.rotacaoX = 0;
+    novo_cubo.rotacaoY = 0;
+    
+    double t = tamanho / 2.0;
+    novo_cubo.vertices = {
+        {-t, -t, -t}, { t, -t, -t}, { t,  t, -t}, {-t,  t, -t},
+        {-t, -t,  t}, { t, -t,  t}, { t,  t,  t}, {-t,  t,  t}
+    };
+    
+    novo_cubo.arestas = {
+        {0, 1}, {1, 2}, {2, 3}, {3, 0},
+        {4, 5}, {5, 6}, {6, 7}, {7, 4},
+        {0, 4}, {1, 5}, {2, 6}, {3, 7}
+    };
+    return novo_cubo;
 }
 
-void movimentar(Poligono& poligono, double distancia, double angulo) {
-	
+void desenhar(Cubo cubo) {
+    glColor3f(0.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+    for (auto& aresta : cubo.arestas) {
+        auto [x1, y1, z1] = cubo.vertices[aresta.first];
+        auto [x2, y2, z2] = cubo.vertices[aresta.second];
+        glVertex3f(x1, y1, z1);
+        glVertex3f(x2, y2, z2);
+    }
+    glEnd();
 }
 
-void escalar(Poligono& poligono, double escala_x, double escala_y) {
-	
+void movimentar(Cubo& cubo, double dx, double dy, double dz) {
+    std::get<0>(cubo.posicao) += dx;
+    std::get<1>(cubo.posicao) += dy;
+    std::get<2>(cubo.posicao) += dz;
+    for (auto& v : cubo.vertices) {
+        std::get<0>(v) += dx;
+        std::get<1>(v) += dy;
+        std::get<2>(v) += dz;
+    }
 }
 
-void rotacionar(Poligono& poligono, double angulo) {
-
+void escalar(Cubo& cubo, double sx, double sy, double sz) {
+    for (auto& v : cubo.vertices) {
+        std::get<0>(v) *= sx;
+        std::get<1>(v) *= sy;
+        std::get<2>(v) *= sz;
+    }
 }
 
-void desenhar(Poligono poligono) {
-	glColor3f(0.0, 0.0, 0.0);
-	glBegin(GL_LINES);
-	for (int i = 0; i < poligono.arestas.size(); i++) {
-		float v_o = poligono.arestas[i].first;
-		float v_d = poligono.arestas[i].second;
-		glVertex2f(poligono.vertices[v_o].first, poligono.vertices[v_o].second);
-		glVertex2f(poligono.vertices[v_d].first, poligono.vertices[v_d].second);
-	}
-	glEnd();
+void rotacionar(Cubo& cubo, double anguloX, double anguloY) {
+    double radX = anguloX * 3.1415926536 / 180.0;
+    double radY = anguloY * 3.1415926536 / 180.0;
+    for (auto& v : cubo.vertices) {
+        double x = std::get<0>(v);
+        double y = std::get<1>(v);
+        double z = std::get<2>(v);
+        
+        double novoY = y * cos(radX) - z * sin(radX);
+        double novoZ = y * sin(radX) + z * cos(radX);
+        std::get<1>(v) = novoY;
+        std::get<2>(v) = novoZ;
+        
+        double novoX = x * cos(radY) + novoZ * sin(radY);
+        novoZ = -x * sin(radY) + novoZ * cos(radY);
+        std::get<0>(v) = novoX;
+        std::get<2>(v) = novoZ;
+    }
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+        case 27: exit(0); break;
+        case 's': escalar(cubo, 1.1, 1.1, 1.1); break;
+        case 'a': escalar(cubo, 0.9, 0.9, 0.9); break;
+    }
+}
+
+void keyboard_special(int key, int x, int y) {
+    switch (key) {
+        case GLUT_KEY_UP: movimentar(cubo, 0, 5, 0); break;
+        case GLUT_KEY_DOWN: movimentar(cubo, 0, -5, 0); break;
+        case GLUT_KEY_LEFT: movimentar(cubo, -5, 0, 0); break;
+        case GLUT_KEY_RIGHT: movimentar(cubo, 5, 0, 0); break;
+        case GLUT_KEY_PAGE_UP: rotacionar(cubo, 5, 0); break;
+        case GLUT_KEY_PAGE_DOWN: rotacionar(cubo, -5, 0); break;
+    }
 }
